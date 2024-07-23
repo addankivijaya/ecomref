85deda3b1d229599810a541344c5986050176321
addankivijaya <124745952+addankivijaya@users.noreply.github.com>
2024-07-23 18:14:05 -0400

    Add files via upload

---
+++ bkp/footer.php
@@ -0,0 +1,390 @@
+ <footer>
+    <!-- BEGIN INFORMATIVE FOOTER -->
+    <div class="footer-inner">
+  <div class="newsletter-row">
+      <div class="container">
+        <div class="row"> 
+    <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12 col"> 
+            <!-- Footer Payment Link -->
+            <div class="payment-accept">
+              <div><img src="<?php echo site_url();?>images/payment-1.png" alt="snacks"> <img src="<?php echo site_url();?>images/payment-2.png" alt="Sweets"> <img src="<?php echo site_url();?>images/payment-3.png" alt="payment3"> <img src="<?php echo site_url();?>images/payment-4.png" alt="Ready to eat"></div>
+            </div>
+          </div>
+          <!-- Footer Newsletter -->
+         <?php /* <div class="col-lg-8 col-md-8 col-sm-8 col-xs-12 col1">
+            <div class="newsletter-wrap">
+              <h4>Sign up for emails</h4>
+              <form action="#" method="post" id="newsletter-validate-detail1">
+                <div id="container_form_news">
+                  <div id="container_form_news2">
+                    <input type="text" name="email" id="newsletter1" title="Sign up for our newsletter" class="input-text required-entry validate-email" placeholder="Enter your email address">
+                    <button type="submit" title="Subscribe" class="button subscribe"><span>Subscribe</span></button>
+                  </div>
+                  <!--container_form_news2--> 
+                </div>
+                <!--container_form_news-->
+              </form>
+            
+            </div>
+            <!--newsletter-wrap--> 
+          </div>*/?>
+          
+        </div>
+      </div>
+      <!--footer-column-last--> 
+    </div>
+      <div class="container">
+        <div class="row">
+             <div class="col-sm-4 col-xs-12 col-lg-4" >
+      <div class="co-info">
+       <h4 style="margin: 11px  0px 0 0px;">Get in touch</h4>
+              <!--<div><a href="index.html"><img src="<?php echo site_url();?>images/logo.png" alt="Mango pickles"></a></div>-->
+              <address>
+              <div><em class="icon-location-arrow"></em><span style="margin: -24px 0 0 20px;">Telugu Pickles<br />Flat No 603, Vetex Paradise, Nizampet X Road, Pin Code : 500072</span></div>
+              <div> <em class="icon-mobile-phone"></em><span>+91 9872469999</span></div>
+              <div> <em class="icon-envelope"></em><span>info@telugupickles.in</span></div>
+              </address>
+        
+            </div>
+      </div>
+      
+        <div class="col-sm-2 col-xs-12 col-lg-2">
+            <div class="footer-column">
+                <h4>Quick Links</h4>
+                <ul class="links">
+										<li class="first"><a title="How to buy" href="<?php echo site_url();?>aboutus">About us</a></li>
+										<li><a href="<?php echo site_url();?>sell">Sell With us</a></li>
+										<li><a href="<?php echo site_url();?>blog">Blog</a></li>
+										<li><a href="<?php echo site_url();?>careers">Careers</a></li>
+										<li><a href="<?php echo site_url();?>faqs">Faqs</a></li>
+										<li class="last"><a title="Return policy" href="<?php echo site_url();?>contactus">Contact us</a></li>
+                </ul>
+            </div>          
+        </div>
+		
+        <div class="col-sm-2 col-xs-12 col-lg-2">
+            <div class="footer-column">
+              <h4>Style Advisor</h4>
+              <ul class="links">
+	                <li class="first"><a  href="<?php echo site_url();?>terms">Terms & Conditions</a></li>
+	                <li><a  href="<?php echo site_url();?>shipping">Shipping Policy</a></li>
+	                <li><a  href="<?php echo site_url();?>privacy">Privacy</a></li>
+	                <li><a  href="<?php echo site_url();?>returns">Returns & Refunds</a></li>                
+              </ul>
+            </div>
+        </div>
+       
+            <div class="col-sm-4 col-xs-12 col-lg-4" style="float: right;">
+              <div class="co-info">
+              <h4 style="margin: 11px  0px 0 -159px;">Social plugins</h4>
+                <div class="social">
+                  <ul class="link">
+	                  <li class="fb pull-left"><a target="_blank" rel="nofollow" href="https://www.facebook.com/Telugu-pickles-324231021313413/" title="Facebook"></a></li>
+	                  <li class="tw pull-left"><a target="_blank" rel="nofollow" href="https://twitter.com/telugu_pickles" title="Twitter"></a></li>
+	                  <li class="googleplus pull-left"><a target="_blank" rel="nofollow" href="https://plus.google.com/u/0/109741725336153865297" title="GooglePlus"></a></li>
+	               
+	                  
+	                 <li class="linkedin pull-left"><a target="_blank" rel="nofollow" href="https://www.linkedin.com/in/telugu-pickles-77119b140/" title="Linkedin"></a></li>
+	                  <li class="youtube pull-left"><a target="_blank" rel="nofollow" href="https://www.youtube.com/channel/UClgsXS_PBmLW--XQMyWcRBw" title="Youtube"></a></li>
+                  </ul>
+                </div>
+            </div>
+      </div>
+        </div>
+       
+    
+      </div>
+    
+      <!--container-->
+    </div>
+    <!--footer-inner-->
+    
+    <div class="footer-middle">
+      <div class="container">
+        <div class="row">
+          <div class="row"> </div>
+        </div>
+        <!--row-->
+      </div>
+      <!--container-->
+    </div>
+    <!--footer-middle-->
+    <div class="footer-bottom">
+      <div class="container">
+        <div class="row">
+          <div class="col-sm-12 col-xs-12 coppyright">© 2017 Telugu pickles. All Rights Reserved.</div>
+         
+        </div>
+        <!--row-->
+      </div>
+      <!--container-->
+    </div>
+    <!--footer-bottom-->
+    <!-- BEGIN SIMPLE FOOTER -->
+  </footer>
+  <!-- End For version 1,2,3,4,6 -->
+  
+</div>
+<!--page-->
+<!-- Mobile Menu-->
+<div id="mobile-menu">
+  <ul>
+    <li>
+      <div class="home"><a href="index.html"><i class="icon-home"></i>Home</a> </div>
+    </li>
+      <li>
+      <div><a href="all-products.html">pickles</a> </div>
+    </li>
+     <li>
+      <div><a href="all-products.html">Spices</a> </div>
+    </li>
+    <li><a href="all-products.html">sweets</a>
+      <ul>
+        
+        <li><a href="all-products.html">Checkout</a>
+          <ul>
+            <li><a href="all-products.html">Checkout Method</a></li>
+            <li><a href="all-products.html">Checkout Billing Info</a></li>
+          </ul>
+        </li>
+          <li><a href="all-products.html">Checkout</a>
+          <ul>
+            <li><a href="all-products.html">Checkout Method</a></li>
+            <li><a href="all-products.html">Checkout Billing Info</a></li>
+          </ul>
+        </li>
+          <li><a href="all-products.html">Checkout</a>
+          <ul>
+            <li><a href="all-products.html">Checkout Method</a></li>
+            <li><a href="all-products.html">Checkout Billing Info</a></li>
+          </ul>
+        </li>
+          <li><a href="all-products.html">Checkout</a>
+          <ul>
+            <li><a href="all-products.html">Checkout Method</a></li>
+            <li><a href="all-products.html">Checkout Billing Info</a></li>
+          </ul>
+        </li>
+       
+        
+      </ul>
+    </li>
+
+    <li><a href="all-products.html">Snacks</a></li>
+    <li><a href="all-products.html">Fast Food</a>
+      <ul>
+        <li> <a href="all-products.html"><span>Indian</span></a>
+    
+          <li> <a href="all-products.html"><span>Aloo Chaat</span></a> </li>
+          <li> <a href="all-products.html"><span>Batata Vada</span></a> </li>
+          <li> <a href="all-products.html"><span>Kachori</span></a> </li>
+          <li> <a href="all-products.html"><span>Panipuri</span></a> </li>
+        </ul>
+        </li>
+   
+    <li><a href="all-products.html">Drinks</a></li>
+  </ul>
+</div>
+<!-- JavaScript -->
+<script type="text/javascript" src="<?php echo site_url();?>js/jquery-1.9.1.min.js"></script>
+<script type="text/javascript" src="<?php echo site_url();?>js/bootstrap.min.js"></script>
+<script type="text/javascript" src="<?php echo site_url();?>js/common.js"></script>
+<script type="text/javascript" src="<?php echo site_url();?>js/jquery.flexslider.js"></script>
+<script type="text/javascript" src="<?php echo site_url();?>js/cloud-zoom.js"></script>
+<script type="text/javascript" src="<?php echo site_url();?>js/owl.carousel.min.js"></script>
+<script type="text/javascript" src="<?php echo site_url();?>js/parallax.js"></script>
+<script type="text/javascript" src="<?php echo site_url();?>js/revslider.js"></script>
+<script type="text/javascript" src="<?php echo site_url();?>js/jquery.bxslider.min.js"></script>
+<script type="text/javascript" src="<?php echo site_url();?>js/jquery.mobile-menu.min.js"></script>
+
+<script type="text/javascript">
+        jQuery(document).ready(function(){
+            jQuery('#thm-rev-slider').show().revolution({
+                dottedOverlay: 'none',
+                delay: 5000,
+                startwidth: 0,
+                startheight:900,
+
+                hideThumbs: 200,
+                thumbWidth: 200,
+                thumbHeight: 50,
+                thumbAmount: 2,
+
+                navigationType: 'thumb',
+                navigationArrows: 'solo',
+                navigationStyle: 'round',
+
+                touchenabled: 'on',
+                onHoverStop: 'on',
+                
+                swipe_velocity: 0.7,
+                swipe_min_touches: 1,
+                swipe_max_touches: 1,
+                drag_block_vertical: false,
+            
+                spinner: 'spinner0',
+                keyboardNavigation: 'off',
+
+                navigationHAlign: 'center',
+                navigationVAlign: 'bottom',
+                navigationHOffset: 0,
+                navigationVOffset: 20,
+
+                soloArrowLeftHalign: 'left',
+                soloArrowLeftValign: 'center',
+                soloArrowLeftHOffset: 20,
+                soloArrowLeftVOffset: 0,
+
+                soloArrowRightHalign: 'right',
+                soloArrowRightValign: 'center',
+                soloArrowRightHOffset: 20,
+                soloArrowRightVOffset: 0,
+
+                shadow: 0,
+                fullWidth: 'on',
+                fullScreen: 'on',
+
+                stopLoop: 'off',
+                stopAfterLoops: -1,
+                stopAtSlide: -1,
+
+                shuffle: 'off',
+
+                autoHeight: 'on',
+                forceFullWidth: 'off',
+                fullScreenAlignForce: 'off',
+                minFullScreenHeight: 0,
+                hideNavDelayOnMobile: 1500,
+            
+                hideThumbsOnMobile: 'off',
+                hideBulletsOnMobile: 'off',
+                hideArrowsOnMobile: 'off',
+                hideThumbsUnderResolution: 0,
+
+                hideSliderAtLimit: 0,
+                hideCaptionAtLimit: 0,
+                hideAllCaptionAtLilmit: 0,
+                startWithSlide: 0,
+                fullScreenOffsetContainer: ''
+            });
+        });
+        </script>
+<script type="text/javascript">
+    function HideMe()
+    {
+        jQuery('.popup1').hide();
+        jQuery('#fade').hide();
+    }
+</script> 
+
+<script type="text/javascript">
+	function addtocart(valu) 
+	{  
+		var quantity = 1;
+		jQuery.ajax({ 
+			 type: "POST", 
+			 url: '<?php echo base_url("cart/insertcart/");?>', 
+			 data : {"quantity" : quantity, "valu" : valu},
+			 complete: function(data)
+			 {  
+				var op = data.responseText.trim();
+				jQuery("#cartdivid").html(op);
+			}
+	 });
+	}
+</script>
+
+
+<script type="text/javascript">
+  function addcart(valu)
+    {
+        var quantity = jQuery("#quantity_id").val();
+        if(quantity == 0)
+        {
+          alert("Please Select Quantity...! Quantity Should be More than 1.");
+        }
+          jQuery.ajax({ 
+             type: "POST", url: '<?php echo site_url();?>cart/insertcart', data: "quantity="+quantity+"&valu="+valu,
+             complete: function(data){  
+                var op = data.responseText.trim();
+               jQuery("#cartdivid").html(op);
+             }
+         });
+    }
+</script>
+
+<script type="text/javascript">
+	function change_price(valu)
+	{
+    var price = jQuery("#price_id").val();
+		op = valu*price;
+		jQuery("#dprice_change_id").html(op);
+	}
+</script>
+
+<script>
+  function deletecart(valu){
+  jQuery.ajax({ 
+     type: "POST", url: '<?php echo site_url();?>products/deletecart', 
+	 data: "valu="+valu,
+     complete: function(data){  
+      var op = data.responseText.trim();
+    jQuery("#cartdivid").html(op);
+     }
+   });
+}
+</script>
+
+
+<!------addtocart---->
+<script type="text/javascript">
+
+    function fly_product(valu)
+    {
+     
+      $("#add-to-cart"+valu).on('click',function(){
+          //Scroll to top if cart icon is hidden on top
+           //alert(valu);
+          $('html, body').animate({
+              'scrollTop' : $(".cart_anchor").position().top
+          });
+          //Select item image and pass to the function
+          var itemImg = $(this).parent().find('img').eq(0);
+          alert(itemImg);
+          flyToElement($(itemImg), $('.cart_anchor'));
+      });
+    }
+
+
+function flyToElement(flyer, flyingTo) {
+  var $func = $(this);
+  var divider = 3;
+  var flyerClone = $(flyer).clone();
+  $(flyerClone).css({position: 'absolute', top: $(flyer).offset().top + "px", left: $(flyer).offset().left + "px", opacity: 1, 'z-index': 1000});
+  $('body').append($(flyerClone));
+  var gotoX = $(flyingTo).offset().left + ($(flyingTo).width() / 2) - ($(flyer).width()/divider)/2;
+  var gotoY = $(flyingTo).offset().top + ($(flyingTo).height() / 2) - ($(flyer).height()/divider)/2;
+   
+  $(flyerClone).animate({
+    opacity: 0.4,
+    left: gotoX,
+    top: gotoY,
+    width: $(flyer).width()/divider,
+    height: $(flyer).height()/divider
+  }, 700,
+  function () {
+    $(flyingTo).fadeOut('fast', function () {
+      $(flyingTo).fadeIn('fast', function () {
+        $(flyerClone).fadeOut('fast', function () {
+          $(flyerClone).remove();
+        });
+      });
+    });
+  });
+}
+
+</script>
+
+<!------addtocart---->
+</body>
+</html>
---
+++ bkp/header.php
@@ -0,0 +1,200 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta http-equiv="Content-Type" content="text/html, charset=UTF-8">
+<title>Andhra Pickles, Sweets, Snacks, Home Foods, Spices At Telugu Pickles</title>
+<meta name="description" content="Andhra Mango pickle, Tomato & Pandu mirchi, Chicken etc,, we are specialized in bringing all famous sweets, Snacks, Home Foods, Spices At Telugu Pickles">
+<meta name="keywords" content="Mango Pickle, Tomato Pickle, Red Chilly Pickle, Dondakaya, Gongura Pandumirchi, Cabbage, Cauliflower, Lemon, Usirikaya, Vegetable Complete, Vankaya, Chiken, Royyalu, Allam, MIxed Vegetable, Kakarakaya, Kothimeera, Dondakaya, Gongura, Redchilly, Tomato, Chakkalu, Chakkidalu, Karampusa, Karamboondi, Mixture, Minapapindi Vadiyalu, Saggubiyyam Vadiyalu, Podina Vadiyalu, Tomato Vadiyalu, Gummadi Vadiyalu, Biyyapupindi Vadiyalu, Biyyapupindi Small Vadiyalu, Ariselu, Kobbari Boorelu, Panchadara Laddu, Bellam Laddu, Gavvalu, Venna Undalu, Pootharekulu, Mamidi Tandra, Kandipappu Karam, Putnulu Karam, Karivepaku Karam, Daniyala Karam, Kobbari Karam, Kakarakaya Karam, Mango Pickle, Narinjathonalu, Marmarala Undalu, Jeedilu, Palli Undalu, Palli Chakka, Kobbari Undalu, Nuvvula Undalu, Thati Tandra, Sweet Amla, Salt Amla, Masala Amla, Imlitofee">
+<meta name="google-site-verification" content="lp_PKLnJYvKXIr2FmRHRnIvIZlzu7SBcgKIsDxf3fCo" />
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-96588165-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
+
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+
+<meta name="robots" content="*">
+<link rel="icon" href="#" type="image/x-icon">
+<link rel="shortcut icon" href="#" type="image/x-icon">
+
+<!-- CSS Style -->
+<link rel="stylesheet" type="text/css" href="<?php echo site_url();?>css/bootstrap.min.css">
+<link rel="stylesheet" type="text/css" href="<?php echo site_url();?>css/font-awesome.css" media="all">
+<link rel="stylesheet" type="text/css" href="<?php echo site_url();?>css/revslider.css" >
+<link rel="stylesheet" type="text/css" href="<?php echo site_url();?>css/owl.carousel.css">
+<link rel="stylesheet" type="text/css" href="<?php echo site_url();?>css/owl.theme.css">
+<link rel="stylesheet" type="text/css" href="<?php echo site_url();?>css/jquery.bxslider.css">
+<link rel="stylesheet" type="text/css" href="<?php echo site_url();?>css/jquery.mobile-menu.css">
+<link rel="stylesheet" type="text/css" href="<?php echo site_url();?>css/style.css" media="all">
+<link rel="stylesheet" type="text/css" href="<?php echo site_url();?>css/responsive.css" media="all">
+<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+
+<link href='https://fonts.googleapis.com/css?family=Open+Sans:400italic,600italic,700italic,400,600,700,800' rel='stylesheet' type='text/css'>
+<link href='https://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
+<link href='https://fonts.googleapis.com/css?family=Roboto+Slab:400,700' rel='stylesheet' type='text/css'>
+<meta name="viewport" content="initial-scale=1.0, width=device-width">
+
+</head>
+<body>
+<div id="page">
+ <header>
+  <div class="header-banner">
+    <div class="assetBlock">
+   
+      <div class="col-md-6">
+         
+          <p style="margin-top: 5px;"><img src="<?php echo site_url();?>images/wlogo.png" class="img-responsive" style="float: left;margin: 4px 0 0 45px;">
+              Call us: <?php echo stripslashes(str_replace("\n","",$web_settings['admin_mobile']))?></p>
+      </div>
+      <div class="col-md-3">
+        <div class="block-wrap-search ">
+            <div class="advanced-search box-radius">
+              <form class="form-inline"  method="post" action="<?php echo site_url();?>home/searchproperty/">
+                <div class="form-group search-input">
+                  <input placeholder="Enter Product Name" type="text" name="keyword" required>
+                </div>
+                <button type="submit" name="submit" value="submit" class="btn-search"><i class="fa fa-search"></i></button>
+              </form>
+            </div>
+          </div>
+      </div>
+      <div class="col-md-3">
+        <div class="block-wrap-search ">
+            <div class="advanced-search box-radius">
+              <form class="form-inline"  method="post" action="<?php echo site_url();?>home/trackorder/">
+                <div class="form-group search-input">
+                  <input placeholder="Enter Your Order Id" type="text" name="orderid" required>
+                </div>
+                <button type="submit" name="submit" value="submit" class="btn-search"><i class="fa fa-search"></i></button>
+              </form>
+            </div>
+          </div>
+      </div>
+     </div>
+  </div>
+  <div id="header">
+    <div class="header-container container">
+    <div class="row">
+        <div class="logo"> 
+			<a href="<?php echo site_url();?>home" title="Flavours HTML">
+				<div>
+					<img src="<?php echo site_url();?>images/logo/<?php echo stripslashes(str_replace("\n","",$web_settings['logo']))?>" alt="telugu pickles">
+				</div>
+			</a> 
+		</div>
+		
+        <div class="fl-nav-menu">
+          <nav>
+            <div class="mm-toggle-wrap">
+              <div class="mm-toggle"><i class="icon-align-justify"></i><span class="mm-label">Menu</span> </div>
+            </div>
+            <div class="nav-inner">
+              <!-- BEGIN NAV -->
+              <ul id="nav" class="hidden-xs">
+                <li id="nav-home" class="level0 parent drop-menu"><a class="level-top active" href="<?php echo site_url();?>home"><span>Home</span></a>
+                </li>
+                <?php foreach($menu as $key => $value){	?>
+                <li class="level0 parent drop-menu"><a href="<?php echo site_url();?>products/prouct_by_category/<?php echo $value['categoryid'];?>"><span><?php echo stripslashes(str_replace("\n","",$value['categoryname'])); ?></span> </a> 
+                <!--sub sub category-->
+                    <ul class="level1">
+					    <?php foreach($value['smenu'] as $skey => $svalue){?>
+                        <li class="level1 first"><a href="<?php echo site_url();?>products/prouct_by_subcategory/<?php echo $value['categoryid'];?>/<?php echo $svalue['sub_cat_id'];?>"><span><?php echo stripslashes(str_replace("\n","",$svalue['sub_cat_name']));?></span></a></li>
+						<?php } ?> 
+                    </ul>
+                </li>
+				<?php } ?>
+              </ul>
+              <!--nav-->
+              </div>
+              </nav>
+        </div>
+        <!--row-->
+      </div>
+      <div class="fl-header-right">
+        <div class="fl-links">
+          <div class="no-js"> <a title="Company" class="clicker"></a>
+            <div class="fl-nav-links">
+          
+              <ul class="links">
+                <?php if($this->session->userdata('user_id')==''){?>
+                <li class="last"><a href="<?php echo site_url();?>login" title="Login"><span>Login</span></a></li>
+				
+                <li><a href="<?php echo site_url();?>register" title="Blog"><span>Register</span></a></li>
+				<?php } ?>
+				
+				<?php if($this->session->userdata('user_id')!=''){?>
+                        <li><a href="#"><i class="fa fa-user"></i> Welcome to <?php echo $this->session->userdata('user_name');?></a>&nbsp;|</li>
+                       <li class="line-nun"><a href="<?php echo site_url(); ?>login/logout"><i class="fa fa-power-off"></i> logout</a></li>
+                    <?php } ?>
+              </ul>
+            </div>
+          </div>
+        </div>
+		
+		<?php if($this->uri->segment(1)!='cart') { ?>
+		
+        <div class="fl-cart-contain">
+		   <div id="cartdivid">
+          <div class="mini-cart">
+		    
+			
+			<?php if($this->cart->total_items()== 0) { ?>
+            <div class="basket"> <a class="cart_anchor" href=""><span>0</span></a> </div>
+			<?php  } else { ?>
+			 <div class="basket"> <a  href=""><span><?php echo $this->cart->total_items();?></span></a> </div>
+            <div class="fl-mini-cart-content" style="display: none;">
+              <div class="block-subtitle">
+                <div class="top-subtotal"><?php echo $this->cart->total_items();?> item(s), <span class="price">$<?php echo number_format($this->cart->total()); ?></span> </div>
+                <!--top-subtotal-->
+                <!--pull-right-->
+              </div>
+              <!--block-subtitle-->
+              <ul class="mini-products-list" id="cart-sidebar">
+                
+				<?php foreach ($this->cart->contents() as $items){ ?>
+                      <?php if ($this->cart->has_options($items['rowid']) == TRUE): ?>
+                      <?php foreach ($this->cart->product_options($items['rowid']) as $option_name => $option_value): ?>
+                       <?php if($option_name=='image'){ ?>
+				<li class="item first">
+                  <div class="item-inner"><a class="product-image"><img alt="spices" src="<?php echo site_url(); ?>images/products/<?php echo $option_value; ?>"></a>
+                    <div class="product-details">
+                      <?php /*<div class="access"><a class="btn-remove1" title="Remove This Item" onClick="deletecart('<?php echo $items['rowid']; ?>');" href="#">Remove</a></div>*/?>
+                      <div class="access"><a class="btn-remove1" title="Remove This Item" href="#">Remove</a> <a class="btn-edit" title="Edit item" href="#" onClick="deletecart('<?php echo $items['rowid']; ?>');"><i class="fa fa-times" aria-hidden="true"></i><span class="hidden">Edit item</span></a> </div>
+                      <!--access-->
+                      <strong><?php echo $items['qty'];?></strong> x <span class="price"><?php echo $items['name'];?></span>
+                      <p class="product-name"><a href=""><?php echo $items['name'];?></a></p>
+                    </div>
+                  </div>
+                </li>
+                <?php }?>
+				<?php endforeach; ?> 
+				<?php endif; ?>
+				<?php }?>
+              </ul>
+              <div class="actions">
+                <div class="col-md-6">
+				    <a href="<?php echo site_url();?>cart"> <button class="btn-checkout" title="Checkout" type="button"><span>Cart</span></button></a>
+				</div>
+
+                <div class="col-md-6">
+				    <a href="<?php echo site_url();?>checkout/payment"><button class="btn-checkout" title="Checkout" type="button"><span>Checkout</span></button></a>
+				</div>
+              </div>
+            </div>
+			<?php } ?> 
+          </div>
+		  </div> 
+        </div>
+		<?php } ?>
+		
+		
+      </div>
+    </div>
+  </div>  
+  </header>
